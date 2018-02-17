e64b63611fc2668305a8f30487c3fea1cb5925e7
hippo604 <mark_matsuno@hotmail.com>
2018-02-16 22:24:19 -0800

    Exercise for Relative Layout

---
+++ RelLayt/RelLayt/RelLayt/RelLayt2.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RelLayt
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class RelLayt2 : ContentPage
+	{
+		public RelLayt2 ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
