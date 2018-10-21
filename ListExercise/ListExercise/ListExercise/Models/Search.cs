a34339242797f573d2a4e822a37164d9ad8a84fd
hippo604 <mark_matsuno@hotmail.com>
2018-10-21 11:07:16 -0700

    Initial checkin of exercises. Got the lists going.
    Signed-off-by: hippo604 <mark_matsuno@hotmail.com>

---
+++ ListExercise/ListExercise/ListExercise/Models/Search.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListExercise.Models
+{
+    public class Search
+    {
+        public string Id { get; set; }
+        public string Location { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+
+
+
+
+    }
+}
---
+++ ListExercise/ListExercise/ListExercise/SearchPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ListExercise.Models;
+
+namespace ListExercise
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SearchPage : ContentPage
+	{
+		public SearchPage ()
+		{
+			InitializeComponent ();
+            var searches = new List<Search>
+            {
+                new Search {Id="1", Location="Hollywood", CheckIn=new DateTime(2018, 04, 14), CheckOut = new DateTime(2018, 04, 15) },
+                new Search {Id="2", Location="Bollywood", CheckIn=new DateTime(2018, 06, 14), CheckOut = new DateTime(2018, 06, 15) },
+                new Search {Id="3", Location="Tokyo", CheckIn=new DateTime(2018, 05, 14), CheckOut = new DateTime(2018, 05, 15) }
+
+
+            };
+
+            listView.ItemsSource = searches;
+		}
+	}
+}
