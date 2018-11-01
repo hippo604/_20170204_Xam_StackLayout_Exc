8deba543cc74957fe4f6b500909130ecb65e9b49
hippo604 <mark_matsuno@hotmail.com>
2018-11-01 06:35:41 -0700

    Works with deletion even in the list filtered based on my search text entry!
    Signed-off-by: hippo604 <mark_matsuno@hotmail.com>

--- ListExercise/ListExercise/ListExercise/Models/SearchService.cs
+++ ListExercise/ListExercise/ListExercise/Models/SearchService.cs
@@ -8,21 +8,25 @@ namespace ListExercise.Models
 {
     class SearchService
     {
-        private IEnumerable<Search> _searches;
-
-        public IEnumerable<Search> GetSearches(string filter = null)
-        {
-            _searches = new List<Search> {
+        private List<Search> _searches = new List<Search> {
                 new Search {Id="1", Location ="Taipei 台北", CheckIn = new DateTime(2018, 04, 14), CheckOut = new DateTime(2018, 04, 15) },
                 new Search {Id="2", Location="Naha 那覇", CheckIn=new DateTime(2018, 04, 14), CheckOut = new DateTime(2018, 04, 15) },
                 new Search {Id="3", Location="Kumamoto 熊本", CheckIn=new DateTime(2018, 06, 14), CheckOut = new DateTime(2018, 06, 15) },
                 new Search {Id="4", Location="Tokyo 東京", CheckIn=new DateTime(2018, 05, 14), CheckOut = new DateTime(2018, 05, 15) }
             };
+
+        public IEnumerable<Search> GetSearches(string filter = null)
+        {
             if (String.IsNullOrWhiteSpace(filter))
                 return _searches;
 
             return _searches.Where(c => c.Location.StartsWith(filter, StringComparison.CurrentCultureIgnoreCase));
         }
 
+        public void DeleteSearch(string searchId)
+        {
+            _searches.Remove(_searches.Single(s => s.Id == searchId));
+           
+        }
     }
 }
--- ListExercise/ListExercise/ListExercise/SearchPageB.xaml.cs
+++ ListExercise/ListExercise/ListExercise/SearchPageB.xaml.cs
@@ -15,26 +15,42 @@ namespace ListExercise
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SearchPageB : ContentPage
     {
-        SearchService searchService = new SearchService();
+        List<SearchGroup3> _searchGroups = new List<SearchGroup3>();
 
+        SearchService searchService = new SearchService();
+               
         void Handle_Textchanged(object sender, TextChangedEventArgs e)
         {
-            listView.ItemsSource = new List<SearchGroup3>()
+            _searchGroups = new List<SearchGroup3>()
             {
                 new SearchGroup3("Recent Searches", searchService.GetSearches(e.NewTextValue))
             };
+
+            listView.ItemsSource = _searchGroups;
+        }
+
+        void Delete_Clicked(object sender, System.EventArgs e)
+        {
+            var menuItem = sender as MenuItem;
+            var search = menuItem.CommandParameter as Search;
+
+            //Deletes from back end
+            searchService.DeleteSearch(search.Id);
+
+            //Delete from front end
+            _searchGroups[0].Remove(search);
         }
 
         public SearchPageB()
         {
             InitializeComponent();
 
-            listView.ItemsSource = new List<SearchGroup3>()
+            _searchGroups = new List<SearchGroup3>()
             {
                 new SearchGroup3("Recent Searches", searchService.GetSearches())
             };
+
+            listView.ItemsSource = _searchGroups;
         }
-
-
     }
 }
