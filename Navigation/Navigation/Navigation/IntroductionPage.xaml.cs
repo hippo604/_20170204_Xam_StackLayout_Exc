4c46b86a4d36e529ed54047d4b62ba67050177c1
hippo604 <mark_matsuno@hotmail.com>
2018-11-18 06:58:10 -0800

    Stack Layout when I worked on it last, checking it in now.
    Signed-off-by: hippo604 <mark_matsuno@hotmail.com>

---
+++ Navigation/Navigation/Navigation/IntroductionPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Navigation
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class IntroductionPage : ContentPage
+	{
+		public IntroductionPage ()
+		{
+			InitializeComponent ();
+		}
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+            //Asynchronously removes the top Page from the navigation stack.
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            //If you want the default behaviour with the back button
+            //return base.OnBackButtonPressed();
+
+            //If you want to block the behaviour of the back button
+            //You need to do this for a modal page
+            return true;
+        }
+    }
+}
---
+++ Navigation/Navigation/Navigation/WelcomePage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Navigation
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class WelcomePage : ContentPage
+	{
+		public WelcomePage ()
+		{
+			InitializeComponent ();
+		}
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            //Each content page has a Navigation property (of type INavigation)
+            //See 2018/1/6 for more details on async/await
+            await Navigation.PushModalAsync(new IntroductionPage());
+        }
+    }
+}
