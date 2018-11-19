6ace8a0c8387f90c7875ba4fb1328752e5a3096a
hippo604 <mark_matsuno@hotmail.com>
2018-11-18 18:52:47 -0800

    Master Detail finished. :)
    Signed-off-by: hippo604 <mark_matsuno@hotmail.com>

---
+++ MasterDetailTwo/MasterDetailTwo/MasterDetailTwo/ContactDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using MasterDetailTwo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MasterDetailTwo
+{
+    public partial class ContactDetailPage : ContentPage
+    {
+        public ContactDetailPage(Contact contact)
+        {
+            if (contact == null)
+                throw new ArgumentNullException();
+
+            BindingContext = contact;
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ MasterDetailTwo/MasterDetailTwo/MasterDetailTwo/ContactsPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using MasterDetailTwo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MasterDetailTwo
+{
+//MasterDetailPage, not a ContentPage
+    public partial class ContactsPage : MasterDetailPage
+    {
+        void Handle_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {            
+            //get currently selected contact
+            var contact = e.SelectedItem as Contact;
+
+            //NavigationPage is needed for back functionality
+            Detail = new NavigationPage(new ContactDetailPage(contact));
+
+            //Decide if the master's presented. False for iPhone.
+            IsPresented = false; 
+
+            //No more nulling needed
+
+        }
+
+        public ContactsPage()
+        {
+            InitializeComponent();
+
+            listView.ItemsSource = new List<Contact> {
+                new Contact { Name = "Mosh", ImageUrl = "http://lorempixel.com/100/100/people/1" },
+                new Contact { Name = "John", ImageUrl = "http://lorempixel.com/100/100/people/2", Status = "Hey, let's talk! AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAA" }
+            };
+        }
+    }
+}
