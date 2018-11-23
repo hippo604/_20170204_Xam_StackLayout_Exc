457a8718c24099284347f53e27fd70b8a0a0d3b5
hippo604 <mark_matsuno@hotmail.com>
2018-11-23 06:06:52 -0800

    Tabbing worked yesterday. Kind of neat.
    Signed-off-by: hippo604 <mark_matsuno@hotmail.com>

---
+++ Tabbed/Tabbed/Tabbed/ContactDetailPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Tabbed.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tabbed
+{
+    //All the same as before
+    public partial class ContactDetailPage : ContentPage
+    {
+        public ContactDetailPage(Contact contact)
+        {
+            if (contact == null)
+                throw new ArgumentNullException();
+
+            BindingContext = contact;
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ Tabbed/Tabbed/Tabbed/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tabbed.Models
+{
+    //All the same as before
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
