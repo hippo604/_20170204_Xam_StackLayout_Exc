aaaa7cc3ba7f2b305f1855e21233fb9e69435e4b
hippo604 <mark_matsuno@hotmail.com>
2019-01-06 07:25:26 -0800

    More progress
    Signed-off-by: hippo604 <mark_matsuno@hotmail.com>

---
+++ FormsSettingPages/FormsSettingPages/FormsSettingPages/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace FormsSettingPages
+{
+
+
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+    }
+}
