41984e35ff0f67c5b29837f18d6d485d70d23ad2
hippo604 <mark_matsuno@hotmail.com>
2019-01-20 10:29:55 -0800

    Finished 1 exercise and added another (Picker with navigation)
    Signed-off-by: hippo604 <mark_matsuno@hotmail.com>

---
+++ FormsSettingPages/FormsSettingPages/FormsSettingPages/Extensions/DateCell2.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FormsSettingPages.Extensions
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DateCell2 : ViewCell
+	{
+        //"Bindable インフラ"; all this is done for stock classes. For instance,
+        //...the Text property (Label class) references the backing field "TextProperty"
+        public static readonly BindableProperty LabelProperty =
+            BindableProperty.Create("Label", typeof(string), typeof(DateCell2));
+
+        public string Label
+        {
+            get {return (string)GetValue(LabelProperty);}
+            set { SetValue(LabelProperty, value); }
+        }
+
+        public DateCell2 ()
+		{
+			InitializeComponent ();
+
+            //We've used BindingContext before in the ContactDetailsPage
+            BindingContext = this;
+		}
+	}
+}
---
+++ PickerwithNav/PickerwithNav/PickerwithNav/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PickerwithNav
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Handle_Tapped(object sender, EventArgs e)
+        {
+
+            var page = new ContactMethodsPage();
+            page.ContactMethods.ItemSelected += (source, args) =>
+            {
+                contactMethod.Text = args.SelectedItem.ToString();
+                Navigation.PopAsync();
+            };
+
+            Navigation.PushAsync(page);
+        }
+    }
+}
