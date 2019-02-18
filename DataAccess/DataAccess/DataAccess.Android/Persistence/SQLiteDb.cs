e639e830cb0546f3de27329280afd05d853bca3e
hippo604 <mark_matsuno@hotmail.com>
2019-02-18 14:56:03 -0800

    Finished last section, starting new section.
    Signed-off-by: hippo604 <mark_matsuno@hotmail.com>

---
+++ DataAccess/DataAccess/DataAccess.Android/Persistence/SQLiteDb.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using SQLite;
+using Xamarin.Forms;
+using DataAcess.Droid;
+using DataAccess;
+
+[assembly: Dependency(typeof(SQLiteDb))]
+
+namespace DataAcess.Droid
+{
+	public class SQLiteDb : ISQLiteDb
+	{
+		public SQLiteAsyncConnection GetConnection()
+		{
+			var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+			var path = Path.Combine(documentsPath, "MySQLite.db3");
+
+			return new SQLiteAsyncConnection(path);
+		}
+	}
+}
+
